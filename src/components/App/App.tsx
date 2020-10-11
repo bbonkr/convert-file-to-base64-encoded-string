0fffca27b74adffb2c24f55415fe1788e2a4a8e3
Pon Cheol Ku <bbon@bbon.kr>
2020-10-11 18:10:26 +0900

    Add File Handling

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,16 +6,21 @@ import { FileInfo, Position } from '../../interfaces';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Content } from '../Content/Content';
-
-import 'halfmoon/css/halfmoon.min.css';
+import { FaTrash, FaDownload } from 'react-icons/fa';
 import { Card } from '../Card/Card';
 import { FileForm } from '../FileForm/FileForm';
+import { Media } from '../Media';
+
+import 'halfmoon/css/halfmoon.min.css';
 
 export const App = () => {
     const [scrollPosition, setScrollPosition] = useState<Position>({
         top: 0,
         left: 0,
     });
+
+    const [files, setFiles] = useState<FileInfo[]>([]);
+
     const handleClickScrollTop = () => {
         setScrollPosition((prevState) => ({
             ...prevState,
@@ -24,8 +29,27 @@ export const App = () => {
         }));
     };
 
-    const handleFileLoaded = (files: FileInfo[]) => {
-        console.info('handleFileLoaded', files);
+    const handleFileLoaded = (loadedFiles: FileInfo[]) => {
+        setFiles((prevState) => {
+            loadedFiles.forEach((x) => {
+                const index = prevState.findIndex((p) => p.id === x.id);
+                if (index < 0) {
+                    prevState.splice(0, 0, x);
+                }
+            });
+            return [...prevState];
+        });
+    };
+
+    const handleRemoveFile = (file: FileInfo) => () => {
+        setFiles((prevState) => {
+            const index = prevState.findIndex((p) => p.id === file.id);
+            if (index >= 0) {
+                prevState.splice(index, 1);
+            }
+
+            return [...prevState];
+        });
     };
 
     useEffect(() => {
@@ -49,6 +73,37 @@ export const App = () => {
                     <Card>
                         <FileForm onFileLoaded={handleFileLoaded} />
                     </Card>
+                </Content>
+                <Content title="Loaded files">
+                    <div className="d-flex flex-row flex-wrap">
+                        {files.map((file) => {
+                            return (
+                                <div key={file.id} className="w-400 mw-full">
+                                    <Card cardClassName="p-0">
+                                        <Media file={file} />
+                                        <Content title={file.name}>
+                                            <p>{file.size} Bytes</p>
+                                            <div className="text-center">
+                                                <button className="btn btn-primary">
+                                                    <FaDownload />{' '}
+                                                    <span>Download</span>
+                                                </button>
+                                                <button
+                                                    className="btn btn-danger"
+                                                    onClick={handleRemoveFile(
+                                                        file,
+                                                    )}
+                                                >
+                                                    <FaTrash />{' '}
+                                                    <span>Remove</span>
+                                                </button>
+                                            </div>
+                                        </Content>
+                                    </Card>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </Content>
             </ContentWrapper>
             <Footer onClickScrollToTop={handleClickScrollTop} />
